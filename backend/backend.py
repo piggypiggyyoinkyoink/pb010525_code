9567eab315a9e15e17cfa61c278b3cad1c030b5b
piggypiggyyoinkyoink <quackquack904@gmail.com>
2025-04-21 17:50:56 +0100

    Tokens are now of the form of JWT tokens, encrypted using asymmetric encryption to prevent forged tokens

--- backend/backend.py
+++ backend/backend.py
@@ -6,20 +6,29 @@ from typing import Annotated, Any
 from contextlib import asynccontextmanager
 from hashlib import sha3_256
 import jwt
+from jwt.exceptions import ExpiredSignatureError
+from cryptography.hazmat.primitives import serialization
+
+private_key = open('.ssh/id_rsa', 'r').read()
+PRIVATE_KEY = serialization.load_ssh_private_key(private_key.encode(), password=b'')
+public_key = open('.ssh/id_rsa.pub', 'r').read()
+PUBLIC_KEY = serialization.load_ssh_public_key(public_key.encode())
 
 fake_users_db = {
     "johndoe": {
         "username": "johndoe",
-        "full_name": "John Doe",
+        "first_name": "John",
+        "last_name": "Doe",
         "email": "johndoe@example.com",
         "hashed_password": "f5a5207a8729b1f709cb710311751eb2fc8acad5a1fb8ac991b736e69b6529a3",
         "disabled": False,
     },
     "alice": {
         "username": "alice",
-        "full_name": "Alice Wonderson",
+        "first_name": "Alice",
+        "last_name": "Inwonderland",
         "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
+        "hashed_password": "74efee2654fbc20723cb7eb381e464be96319c23064a4ffce86a5de8470dd863",
         "disabled": True,
     },
 }
@@ -28,7 +37,9 @@ fake_users_db = {
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     print("Hello")
-    print(sha3_256(bytes("secret", "utf-8")).hexdigest())
+    global APPLES
+    APPLES = 7
+    print(sha3_256(bytes("secret2", "utf-8")).hexdigest())
     yield
     print("Goodbye")
 
@@ -54,7 +65,8 @@ app.add_middleware(
 class User(BaseModel):
     username: str
     email: str | None = None
-    full_name: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
     disabled: bool | None = None
 
 class UserInDB(User):
@@ -70,14 +82,22 @@ def get_user(db, username: str):
         user_dict = db[username]
         return UserInDB(**user_dict)
     
-def fake_decode_token(token):
-    return User(
-        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
-    )
+def hgg(db, username:str):
+    if username in db:
+        user_dict = db[username]
+        return User(**user_dict)
+    
+def decode_token(token):
+    try:
+        payload = jwt.decode(jwt=token, key=PUBLIC_KEY, algorithms=['RS256', ])
+        print(payload)
+        return get_user(fake_users_db, payload["sub"])
+    except ExpiredSignatureError:
+        raise ExpiredSignatureError
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2Scheme)]):
-    user = fake_decode_token(token)
+    user = decode_token(token)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials", headers={"WWW-Authenticate": "Bearer"})
     return user
@@ -98,6 +118,7 @@ async def root():
 @app.post("/token")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user_dict = fake_users_db.get(form_data.username)
+    print(user_dict)
     if not user_dict:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     user = UserInDB(**user_dict)
@@ -105,7 +126,11 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     if not hashed_password == user.hashed_password:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
 
-    return {"access_token": user.username, "token_type": "bearer"}
+    jwt_payload = {"sub": user.username}
+    
+    token = jwt.encode(payload=jwt_payload, key=PRIVATE_KEY, algorithm="RS256")
+    print(token)
+    return {"access_token": token, "token_type": "bearer"}
 
 
 @app.get("/users/me")
