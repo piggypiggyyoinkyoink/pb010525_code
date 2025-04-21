88fbb89d885eb3632e10343fcb068d1d2b55c56b
piggypiggyyoinkyoink <quackquack904@gmail.com>
2025-04-21 17:11:13 +0100

    backend: rudimentary auth system setup; still need to sort out tokens

--- backend/backend.py
+++ backend/backend.py
@@ -1,10 +1,41 @@
-from fastapi import FastAPI, status, Query, Path, Body
+from fastapi import FastAPI, HTTPException, status, Depends, Query, Path, Body
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel, Field, EmailStr
 from typing import Annotated, Any
+from contextlib import asynccontextmanager
+from hashlib import sha3_256
+import jwt
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "f5a5207a8729b1f709cb710311751eb2fc8acad5a1fb8ac991b736e69b6529a3",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    print("Hello")
+    print(sha3_256(bytes("secret", "utf-8")).hexdigest())
+    yield
+    print("Goodbye")
+
+
+app = FastAPI(lifespan=lifespan)
+
+oauth2Scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 origins = [
     "http://localhost:5173",
@@ -19,6 +50,66 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+
+
+def hash_password(password: str):
+    return sha3_256(bytes(password, "utf-8")).hexdigest()
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+    
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2Scheme)]):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials", headers={"WWW-Authenticate": "Bearer"})
+    return user
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def root():
     return {"message":"Hello World"}
+
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
